af52aaae857b58dba00996772318becee395c94b
fushengyuanwuhui <1138128021@qq.com>
2020-09-19 20:14:36 +0800

    Initial commit

---
+++ src/main/java/com/gengdan/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.gengdan.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author ShiJiaWei
+ * @version 1.0
+ * @date 2020/9/15 09:00
+ */
+
+@Controller
+public class UserController {
+
+    @RequestMapping("login")
+    String showLoginForm(){
+        System.out.println("showLoginForm");
+        return "login";
+    }
+
+    @GetMapping("loginAction")
+    String loginAction(@RequestParam("username") String userName,
+                       @RequestParam("password") String userPassword, Model model){
+        String textUserNameValue="pig";
+        String testUserPasswordValue="123456";
+        return "";
+    }
+}

d72e9612d33244568f283b97d956aaede044d783
YimaiDuan <dym0822@163.com>
2020-11-03 08:20:54 +0800

    10.27

--- src/main/java/com/gengdan/demo/entity/User.java
+++ src/main/java/com/gengdan/demo/entity/User.java
@@ -3,26 +3,22 @@ package com.gengdan.demo.entity;
 
 import lombok.Data;
 
+/**
+ * @author inter.d
+ */
 @Data
 public class User {
 
-  private long id;
+  private Integer id;
   private String userName;
   private String password;
   private String realName;
 
-  public User(String userName, String password, String realName) {
+  public User( Integer id,String userName, String password, String realName) {
+    this.id = id;
     this.userName = userName;
     this.password = password;
     this.realName = realName;
-  }
-
-  public User(long id) {
-    this.id = id;
-  }
-
-  public User(){
 
   }
-
 }
